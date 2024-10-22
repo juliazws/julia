46a34d7d3c1cfa8686665b3be29e9ac53c2397af
juliazws <162149276+juliazws@users.noreply.github.com>
2024-10-22 11:51:31 +0000

    alteracoes

---
+++ perguntas.js
@@ -0,0 +1,17 @@
+criaCartao(
+    'programação',
+    'o que é pithon?'
+    'o pithon é uma linguagem de programação'
+)
+
+criaCartao (
+    'geografia'
+    'qual é a capital da frança?'
+    'a capital da frnça é paris'
+)
+
+criaCartao (
+    'programação'
+    'o que é função'
+    'uma função é um bloco de código que executa alguma tarefa'
+)
